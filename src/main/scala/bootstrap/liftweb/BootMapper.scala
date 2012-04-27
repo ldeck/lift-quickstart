a8a93d14b82707a47f6f936ea4e9933f5b2bcc62
Lachlan Deck <lachlan.deck@gmail.com>
2012-04-28 07:35:30 +1000

    BootMapper conveniences: allowing db.schema prop, default to snakifying column_names, printing sys.error for failed migrations

--- src/main/scala/bootstrap/liftweb/BootMapper.scala
+++ src/main/scala/bootstrap/liftweb/BootMapper.scala
@@ -21,15 +21,33 @@ object BootMapper {
       val vendor = new StandardDBVendor(Props.get("db.driver") openOr "org.h2.Driver",
                                         Props.get("db.url") openOr "jdbc:h2:lift_proto.db;AUTO_SERVER=TRUE",
                                         Props.get("db.user"),
-                                        Props.get("db.password"))
+                                        Props.get("db.password")
+                                        Props.get("db.schema"))
 
       LiftRules.unloadHooks.append(vendor.closeAllConnections_! _)
       DB.defineConnectionManager(DefaultConnectionIdentifier, vendor)
     }
 
+    // Mapper default rules
+    MapperRules.columnName = (_,name) => StringHelpers.snakify(name)
+    MapperRules.tableName  = (_,name) => StringHelpers.snakify(name)
+
     // Use Lift's Mapper ORM to populate the database
     // you don't need to use Mapper to use Lift... use
     // any ORM you want
-    Schemifier.schemify(true, Schemifier.infoF _, User)
+    val cmds = Schemifier.schemify(true, Schemifier.infoF _, User)
+    if (!cmds.isEmpty)
+    {
+      sys.error("Database scheme is out of date. The following is missing:\n" + cmds.mkString("\n"))
+    }
   }
+}
+
+class DBVendor(val driverName: String, val dbUrl: String, val dbUser: Box[String], val dbPassword: Box[String], val schemaName: Box[String] = Empty)
+  extends StandardDBVendor(driverName, dbUrl, dbUser, dbPassword)
+{
+  Class.forName(driverName)
+
+  override def newSuperConnection(name: ConnectionIdentifier) : Box[SuperConnection] =
+    newConnection(name).map(c => new SuperConnection(c, () => releaseConnection(c), tryo(schemaName openOr "PUBLIC")))
 }
