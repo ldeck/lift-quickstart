ae129f3c9fb8e4930121ffa9f7f0a64ba8d95210
Lachlan Deck <lachlan.deck@gmail.com>
2012-04-28 11:29:25 +1000

    call custom DBVendor rather than StandardDBVendor to allow an optional schema name.

--- src/main/scala/bootstrap/liftweb/BootMapper.scala
+++ src/main/scala/bootstrap/liftweb/BootMapper.scala
@@ -18,11 +18,11 @@ import code.model._
 object BootMapper {
   def boot {
     if (!DB.jndiJdbcConnAvailable_?) {
-      val vendor = new StandardDBVendor(Props.get("db.driver") openOr "org.h2.Driver",
-                                        Props.get("db.url") openOr "jdbc:h2:lift_proto.db;AUTO_SERVER=TRUE",
-                                        Props.get("db.user"),
-                                        Props.get("db.password")
-                                        Props.get("db.schema"))
+      val vendor = new DBVendor(Props.get("db.driver") openOr "org.h2.Driver",
+                                Props.get("db.url") openOr "jdbc:h2:lift_proto.db;AUTO_SERVER=TRUE",
+                                Props.get("db.user"),
+                                Props.get("db.password"),
+                                Props.get("db.schema"))
 
       LiftRules.unloadHooks.append(vendor.closeAllConnections_! _)
       DB.defineConnectionManager(DefaultConnectionIdentifier, vendor)
